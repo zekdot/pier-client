de573cf42d05df4364e5342a76fcfb4b91e321c2
zekdot <452526076@qq.com>
2022-04-06 22:08:57 +0800

    change interchainGet to v1.6.5 way

--- cli_client/rpc_client.go
+++ cli_client/rpc_client.go
@@ -15,6 +15,12 @@ type ReqArgs struct {
 	FuncName string
 	Args []string
 }
+const (
+	delimiter = "&"
+	outmeta = "outter-meta"
+	inmeta = "inner-meta"
+	callbackmeta = "callback-meta"
+)
 
 func NewRpcClient(address string) (*RpcClient, error) {
 	rpcClient, err := rpc.DialHTTP("tcp", address)
@@ -27,28 +33,17 @@ func NewRpcClient(address string) (*RpcClient, error) {
 }
 
 func (rpcClient *RpcClient) Init() error {
-	var reply string
-	reqArgs := ReqArgs{
-		"init",
-		[]string{"inner-meta", "{}"},
-	}
-	err := rpcClient.client.Call("Service.SetValue", reqArgs, &reply)
+ 	err := rpcClient.SetData(inmeta, "{}")
 	if err != nil {
 		return err
 	}
-	reqArgs = ReqArgs{
-		"init",
-		[]string{"outter-meta", "{}"},
-	}
-	err = rpcClient.client.Call("Service.SetValue", reqArgs, &reply)
+
+	err = rpcClient.SetData(outmeta, "{}")
 	if err != nil {
 		return err
 	}
-	reqArgs = ReqArgs{
-		"init",
-		[]string{"callback-meta", "{}"},
-	}
-	err = rpcClient.client.Call("Service.SetValue", reqArgs, &reply)
+
+	err = rpcClient.SetData(callbackmeta, "{}")
 	if err != nil {
 		return err
 	}
@@ -89,64 +84,91 @@ type Event struct {
 	Func          string `json:"func"`
 	Args          string `json:"args"`
 	Callback      string `json:"callback"`
+	Argscb        string `json:"argscb"`
+	Rollback      string `json:"rollback"`
+	Argsrb        string `json:"argsrb"`
+	Proof         []byte `json:"proof"`
+	Extra         []byte `json:"extra"`
 }
 
-func (rpcClient *RpcClient) InterchainGet(toId string, contractId string, key string) error {
-	cid := "mychannel&data_swapper"
-	//destChainId := toId
-	var reply string
-	reqArgs := ReqArgs{
-		"get",
-		[]string{"outter-meta"},
-	}
-	err := rpcClient.client.Call("Service.GetValue", reqArgs, &reply)
+func (rpcClient *RpcClient) getMeta(key string) (map[string]uint64, error) {
+	reply, err := rpcClient.GetData(key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	outMeta := make(map[string]uint64)
 	err = json.Unmarshal([]byte(reply), &outMeta)
 	if err != nil {
+		return nil, err
+	}
+	return outMeta, nil
+}
+
+func (rpcClient *RpcClient) GetOuterMeta() (map[string]uint64, error) {
+	return rpcClient.getMeta(outmeta)
+}
+
+func (rpcClient *RpcClient) EmitInterchainEvent(args []string) error {
+	if len(args) != 8 {
+		return fmt.Errorf("incorrect number of arguments, expecting 8")
+	}
+	if len(args[0]) == 0 || len(args[1]) == 0{
+		return fmt.Errorf("incorrect nil destination appchain id or destination contract address")
+	}
+
+	destChainID := args[0]
+	outMeta, err := rpcClient.GetOuterMeta()
+
+	if _, ok := outMeta[destChainID]; !ok {
+		outMeta[destChainID] = 0
+	}
+
+	//cid, err := getChaincodeID(stub)
+	cid := "mychannel&broker"
+	if err != nil {
 		return err
 	}
-	if _, ok := outMeta[toId]; !ok {
-		outMeta[toId] = 0
-	}
+
 	tx := &Event{
-		Index:         outMeta[toId] + 1,
-		DstChainID:    toId,
+		Index:         outMeta[destChainID] + 1,
+		DstChainID:    destChainID,
 		SrcContractID: cid,
-		DstContractID: contractId,
-		Func:          "interchainGet",
-		Args:          key,
-		Callback:      "interchainSet",
+		DstContractID: args[1],
+		Func:          args[2],
+		Args:          args[3],
+		Callback:      args[4],
+		Argscb:        args[5],
+		Rollback:      args[6],
+		Argsrb:        args[7],
 	}
+
+	outMeta[tx.DstChainID]++
+
 	txValue, err := json.Marshal(tx)
+	if err != nil {
+		return err
+	}
+
+	// persist out message
 	outKey := outMsgKey(tx.DstChainID, strconv.FormatUint(tx.Index, 10))
-	reqArgs = ReqArgs{
-		"set",
-		[]string{outKey, string(txValue)},
+	if err := rpcClient.SetData(outKey, string(txValue)); err != nil {
+		return err
 	}
+
+	// save outter-meta
+	metaStr, err := json.Marshal(outMeta)
 	if err != nil {
 		return err
 	}
-	err = rpcClient.client.Call("Service.SetValue", reqArgs, &reply);
-	if err != nil {
-		return err
-	}
-	outMeta[toId] ++
-	outMetaBytes, err := json.Marshal(outMeta)
-	if err != nil {
-		return err
-	}
-	reqArgs = ReqArgs{
-		"set",
-		[]string{"outter-meta", string(outMetaBytes)},
-	}
-	err = rpcClient.client.Call("Service.SetValue", reqArgs, &reply)
-	if err != nil {
+	if err := rpcClient.SetData(outmeta, string(metaStr)); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (rpcClient *RpcClient) InterchainGet(toId string, contractId string, key string) error {
+	args := []string{toId, contractId, "interchainGet", key, "interchainSet", key, "", ""}
+	return rpcClient.EmitInterchainEvent(args)
 }
 
 func outMsgKey(to string, idx string) string {
--- rpc_client.go
+++ rpc_client.go
@@ -115,64 +115,6 @@ func (rpcClient *RpcClient) InterchainGet(args[] string) (string, error) {
 
 /************* start of cross-chain related method area **************/
 
-func (rpcClient *RpcClient) EmitInterchainEvent(args []string) error {
-	if len(args) != 8 {
-		return fmt.Errorf("incorrect number of arguments, expecting 8")
-	}
-	if len(args[0]) == 0 || len(args[1]) == 0{
-		return fmt.Errorf("incorrect nil destination appchain id or destination contract address")
-	}
-
-	destChainID := args[0]
-	outMeta, err := rpcClient.GetOuterMeta()
-
-	if _, ok := outMeta[destChainID]; !ok {
-		outMeta[destChainID] = 0
-	}
-
-	//cid, err := getChaincodeID(stub)
-	cid := "mychannel&broker"
-	if err != nil {
-		return err
-	}
-
-	tx := &Event{
-		Index:         outMeta[destChainID] + 1,
-		DstChainID:    destChainID,
-		SrcContractID: cid,
-		DstContractID: args[1],
-		Func:          args[2],
-		Args:          args[3],
-		Callback:      args[4],
-		Argscb:        args[5],
-		Rollback:      args[6],
-		Argsrb:        args[7],
-	}
-
-	outMeta[tx.DstChainID]++
-
-	txValue, err := json.Marshal(tx)
-	if err != nil {
-		return err
-	}
-
-	// persist out message
-	outKey := outMsgKey(tx.DstChainID, strconv.FormatUint(tx.Index, 10))
-	if err := rpcClient.SetData(outKey, string(txValue)); err != nil {
-		return err
-	}
-
-	// save outter-meta
-	metaStr, err := json.Marshal(outMeta)
-	if err != nil {
-		return err
-	}
-	if err := rpcClient.SetData(outmeta, string(metaStr)); err != nil {
-		return err
-	}
-	return nil
-}
-
 func (rpcClient *RpcClient) InvokeIndexUpdate(sourceChainID, sequenceNum string, isReq bool) error {
 	if err := rpcClient.updateIndex(sourceChainID, sequenceNum, isReq); err != nil {
 		return err
@@ -391,15 +333,15 @@ func (rpcClient *RpcClient) getMeta(key string) (map[string]uint64, error) {
 }
 
 func (rpcClient *RpcClient) GetInnerMeta() (map[string]uint64, error) {
-	return rpcClient.getMeta("inner-meta")
+	return rpcClient.getMeta(inmeta)
 }
 
 func (rpcClient *RpcClient) GetOuterMeta() (map[string]uint64, error) {
-	return rpcClient.getMeta("outter-meta")
+	return rpcClient.getMeta(outmeta)
 }
 
 func (rpcClient *RpcClient) GetCallbackMeta() (map[string]uint64, error) {
-	return rpcClient.getMeta("callback-meta")
+	return rpcClient.getMeta(callbackmeta)
 }
 
 func outMsgKey(to string, idx string) string {
