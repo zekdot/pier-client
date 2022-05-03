b5be6454ca7138c41a4b575c5d87a88c3fbcfac8
zekdot <452526076@qq.com>
2022-05-04 00:00:05 +0800

    modify fabric-broker-service to interact with leveldb of pier too

--- broker_service/fabric/constants.go
+++ broker_service/fabric/constants.go
@@ -3,6 +3,7 @@ package main
 const (
 	Addr = "localhost:7053"
 	OrganizationsPath = "/home/zekdot/bitxhub/fabric-samples/test-network/organizations"
+	DB_PATH = "/home/ghl/.pier/meta-data"
 	Username = "appuser"
 	CCID = "broker"
 	ChannelId =  "mychannel"
--- broker_service/fabric/rpc_server.go
+++ broker_service/fabric/rpc_server.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/hashicorp/go-hclog"
+	"github.com/syndtr/goleveldb/leveldb"
 	"os"
+	"strconv"
 
 	//"github.com/wonderivan/logger"
 
 	//"github.com/hashicorp/go-hclog"
 	//"os"
 )
-
+const (
+	delimiter = "&"
+	outmeta = "outter-meta"
+	inmeta = "inner-meta"
+	callbackmeta = "callback-meta"
+)
 var (
 	logger = hclog.New(&hclog.LoggerOptions{
 		Name:   "performance",
@@ -34,8 +42,16 @@ type Event struct {
 }
 type Service struct {
 	broker *BrokerClient
+	db *leveldb.DB
 }
 
+func NewDB(path string) (*leveldb.DB, error) {
+	db, err := leveldb.OpenFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
 
 type ReqArgs struct {
 	FuncName string
@@ -43,8 +59,13 @@ type ReqArgs struct {
 }
 
 func NewService(broker *BrokerClient) *Service {
+	db, err := NewDB(DB_PATH)
+	if err != nil {
+		panic(err)
+	}
 	return &Service{
 		broker: broker,
+		db: db,
 	}
 }
 
@@ -77,4 +98,80 @@ func (s *Service) GetValue(req *ReqArgs, reply *string) error{
 	res, err := broker.getValue(args[0])
 	*reply = string(res)
 	return err
+}
+
+func outMsgKey(to string, idx string) string {
+	return fmt.Sprintf("out-msg-%s-%s", to, idx)
+}
+
+// query transaction and need result
+func (s *Service) Init(req *ReqArgs, reply *string) error{
+	emptyMap := make(map[string]uint64)
+	var emptyMapBytes []byte
+	emptyMapBytes, err := json.Marshal(emptyMap)
+	if err != nil {
+		return err
+	}
+	batch := new(leveldb.Batch)
+	batch.Put([]byte(inmeta), emptyMapBytes)
+	batch.Put([]byte(outmeta), emptyMapBytes)
+	batch.Put([]byte(callbackmeta), emptyMapBytes)
+	return s.db.Write(batch, nil)
+}
+
+func (s *Service) InterchainGet(req *ReqArgs, reply *string) error {
+	args := req.Args
+
+	destChainID := args[0]
+	contractId := args[1]
+	key := args[2]
+	outMetaBytes, err := s.db.Get([]byte(outmeta), nil)// rpcClient.GetOuterMeta()
+	outMeta := make(map[string]uint64)
+	if err = json.Unmarshal(outMetaBytes, outMeta); err != nil {
+		return err
+	}
+	if _, ok := outMeta[destChainID]; !ok {
+		outMeta[destChainID] = 0
+	}
+
+	//cid, err := getChaincodeID(stub)
+	cid := "mychannel&broker"
+	if err != nil {
+		return err
+	}
+	// toId, contractId, "interchainGet", key, "interchainSet", key, "", ""
+	tx := &Event{
+		Index:         outMeta[destChainID] + 1,
+		DstChainID:    destChainID,
+		SrcContractID: cid,
+		DstContractID: contractId,
+		Func:          "interchainGet",
+		Args:          key,
+		Callback:      "interchainSet",
+		Argscb:        key,
+		Rollback:      "",
+		Argsrb:        "",
+	}
+
+	outMeta[tx.DstChainID]++
+
+	txValue, err := json.Marshal(tx)
+	if err != nil {
+		return err
+	}
+
+	// use batch update
+
+	// persist out message
+	outKey := outMsgKey(tx.DstChainID, strconv.FormatUint(tx.Index, 10))
+	metaBytes, err := json.Marshal(outMeta)
+
+	logger.Info("s1:key-" + key +" save cross-chain request to ledger")
+	batch := new(leveldb.Batch)
+	batch.Put([]byte(outKey), txValue)
+	batch.Put([]byte(outmeta), metaBytes)
+	defer logger.Info("s2:key-" + key +" have saved cross-chain request to ledger")
+
+
+	return s.db.Write(batch, nil)
 }
