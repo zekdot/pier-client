12000afd8f1e0ee9ce2b80e8490980754dccf445
zekdot <452526076@qq.com>
2022-12-01 00:03:29 +0800

    add authorization method to appchain_client

--- appchain_client/http/constants.go
+++ appchain_client/http/constants.go
@@ -1,3 +1,8 @@
 package main
 
+const DATA_URL = "http://localhost:3001/api/v1/sawtooth/tp/custom-package/getData"
+const TOKEN_URL = "http://localhost:3001/api/v1/identity/token"
+
 const RPC_PORT = "1211"
+const EMAIL = ""
+const PASSWORD = ""
--- appchain_client/http/http_client.go
+++ appchain_client/http/http_client.go
@@ -4,23 +4,54 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"encoding/json"
 )
 
 type HttpClient struct {
 	client *http.Client
+	token string
 }
 
 func NewAppchainClient() *AppchainClient {
 	var httpClient AppchainClient = &HttpClient{
 		client: &http.Client{},
+		token: "",
 	}
 	return &httpClient
 }
 
-const DATA_URL = "http://localhost:3005/getCustomPackage"
+// try to login with configured username and password
+func (httpClient *HttpClient) Login() (string, error) {
+	req, err := http.PostForm(TOKEN_URL, url.Values{
+		"email":{ EMAIL },
+		"password":{PASSWORD},
+	})
+
+	if err != nil {
+		return "", err
+	}
+	// res, err := httpClient.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer req.Body.Close()
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return "", err
+	}
+	var dataMap map[string]string
+	err = json.Unmarshal(body, &dataMap)
+	if err != nil {
+		return "", err
+	}
+	return dataMap["token"], nil
+}
 
 func (httpClient *HttpClient) GetValue(consumerPackageId string) (string, error){
-	req, err := http.NewRequest("GET", DATA_URL + "?consumerPackageId=" + consumerPackageId, nil)
+	req, err := http.NewRequest("GET", DATA_URL + "/" + consumerPackageId, nil)
+	req.Header.Set("Authorization", "Bearer " + httpClient.token)
 	if err != nil {
 		return "", err
 	}
@@ -33,6 +64,17 @@ func (httpClient *HttpClient) GetValue(consumerPackageId string) (string, error)
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return "", err
+	}
+	if string(body) == "{\"statusCode\":401,\"message\":\"Unauthorized\"}" {
+		var err error
+		httpClient.token, err = httpClient.Login()
+		if err != nil {
+			return "", nil
+		}
+		req, _ := http.NewRequest("GET", DATA_URL + "/" + consumerPackageId, nil)
+		req.Header.Set("Authorization", "Bearer " + httpClient.token)
+		res, _ := httpClient.client.Do(req)
+		body, err = ioutil.ReadAll(res.Body)
 	}
 	return string(body), nil
 }
